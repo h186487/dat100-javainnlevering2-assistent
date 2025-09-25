c5b28e51d86314985afc002ed43779f221edf45f
h186487 <nadia.lambrecht@gmail.com>
2025-09-25 11:09:21 +0200

    oppgaver matriser

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -5,32 +5,61 @@ public class Matriser {
 	// a)
 	public static void skrivUt(int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[i].length; j++) {
+                System.out.print(matrise[i][j] + " ");
+            }
+        }
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		String result = "";
+        for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[i].length; j++) {
+                result += matrise[i][j] + " ";
+            }
+            result += "\n";
+        }
+        return result;
 		
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
+
+        int[][] nyMatrise = new int[matrise.length][];
+
+        for (int i = 0; i < matrise.length; i++) {
+            nyMatrise[i] = new int[matrise[i].length];
+            for (int j = 0; j < matrise[i].length; j++) {
+                nyMatrise[i][j] = matrise[i][j] * tall;
+            }
+        }
+        return nyMatrise;
 	
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
-		
+
+        if (a.length != b.length) {
+            return false;
+        }
+		for (int i = 0; i < a.length; i++) {
+            if(a[i].length != b[i].length) {
+                return false;
+            }
+            for (int j = 0; j < a[i].length; j++) {
+                if(a[i][j] != b[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
 	}
 	
 	// e)
